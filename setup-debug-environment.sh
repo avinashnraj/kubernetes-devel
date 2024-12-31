a6e0fc425637e0bccfec86de3865da7eea1a0efc
Avinash Raj <avinash.nraj@gmail.com>
2024-12-31 06:55:35 -0500

    setting the gopath

--- setup-debug-environment.sh
+++ setup-debug-environment.sh
@@ -28,13 +28,12 @@ apt-get update && apt-get install -y \
 # Set up Go environment
 echo "Setting up Go environment..."
 export GO_VERSION="1.23.0"
-export GOPATH="/go"
-export PATH="$GOPATH/bin:/usr/local/go/bin:/app/kubernetes/_output/bin:$PATH"
-
 wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz && \
     tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz && \
     rm go${GO_VERSION}.linux-amd64.tar.gz
 
+export GOPATH="/go"
+export PATH="${GOPATH}/bin:/usr/local/go/bin:/app/kubernetes/_output/bin:${PATH}"
 mkdir -p $GOPATH/src $GOPATH/bin && \
     chmod -R 777 $GOPATH
 
@@ -42,6 +41,10 @@ go install golang.org/x/tools/gopls@latest && \
     go install honnef.co/go/tools/cmd/staticcheck@latest
 
 go install github.com/go-delve/delve/cmd/dlv@latest
+
+echo 'export GOPATH="/go"' >> ~/.bashrc
+echo 'export PATH="${GOPATH}/bin:/usr/local/go/bin:/app/kubernetes/_output/bin:${PATH}"' >> ~/.bashrc
+source ~/.bashrc
 
 # Print completion message
 echo "Debug environment setup is complete!"
